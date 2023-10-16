f61de6747c17a5a8803af6332e05515b62030bb2
Mauritz Marais <mauritz303@gmail.com>
2023-10-16 15:44:51 +0200

    create game image component and functionality to get image dimensions

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,50 +1,70 @@
 'use client'
 import Nav from "../components/nav"
+import GameTimer from "../components/timer"
 import pic1 from "../../../public/images/pic-1.jpg"
 import pic2 from "../../../public/images/pic-2.jpg"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef, useLayoutEffect } from "react"
 import Image from "next/image"
 
 export default function Game() {
+    const ref = useRef(null);
 
     // start game state
     const [gameStart, setGameStart] = useState(false);
     // game image
     const [gameImage, setGameImage] = useState(pic1);
 
+    // number of characters found state*
+    // if all characters found update state, ui etc*
+
     // paused state
     const [gamePause, setGamePause] = useState(false);
 
     // popup box coords and z-index
-    const [popupCoords, setPopupCoords] = useState({ x: 5, y: 5 });
+    const [clickCoords, setClickCoords] = useState({ x: 5, y: 5 });
     const [popupZIndex, setPopupZIndex] = useState(-10);
+
+    // image width (pass set function to gameimage component)
+    const [imageWidth, setImageWidth] = useState(0);
+    const [imageHeight, setImageHeight] = useState(0);
 
     // handle starting game
     function handleGameStart(img) {
         setGameStart(true);
         setGameImage(img);
+
+        // send request to server to record start time*
     }
+
+    // handle end game
+    // send request to server to record end time and calculate game time*
 
 
     // clicking on image displays options box at cursor
     function setCoords(e) {
 
-        console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
         // update position of box
-        setPopupCoords({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
-        // change z-index of box
-        (popupZIndex === -10 ? setPopupZIndex(10) : setPopupZIndex(-10));
+        setClickCoords({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
 
-        // pause or resume game (not needed?)
-        // (gamePause ? setGamePause(false) : setGamePause(true));
+        // change z-index of box based on current z-index
+        (popupZIndex === -10 ? setPopupZIndex(10) : setPopupZIndex(-10));
     }
 
     // selecting character from box
     function handleFormSubmit(e) {
         e.preventDefault();
-        // check if selection is correct* (fetch)
+
+        // 1. get coords of cursor/click*
+        // calculate x and y values*
+        let x_percentage = clickCoords.x / imageWidth;
+
+        console.log(Math.floor(x_percentage * 100));
+
+        // 2. make call to backend*(check if values are within bounds)
+
         // handle result, update game state*
-        // change z-index of popup*
+
+        // change z-index of popup
         setPopupZIndex(-10);
 
     }
@@ -58,6 +78,7 @@ export default function Game() {
                     <h1 className="font-bold text-2xl">Game page</h1>
                     <p>select image</p>
                     <div className="flex gap-4">
+
                         <picture className="w-96 h-96" onClick={() => handleGameStart(pic1)}>
                             <Image src={pic1.src} alt="" width={500} height={500} className="w-full h-full" />
                             {/* <img className="w-full h-full" src={pic1.src} alt="" /> */}
@@ -78,12 +99,13 @@ export default function Game() {
                 <main className="flex flex-col items-center p-4">
                     <GameTimer gameStart={gameStart} gamePause={gamePause} />
                     <h1 className="font-bold text-2xl">Game page</h1>
+
                     <div className="relative w-full h-full">
-                        <picture className="w-full h-full" onClick={(e) => setCoords(e)}>
-                            <Image src={gameImage.src} alt="" width={800} height={800} className="w-full h-full" />
-                            {/* <img className="w-full h-full" src={pic1.src} alt="" /> */}
-                        </picture>
-                        <SelectOptions x={popupCoords.x} y={popupCoords.y} z={popupZIndex} handleFormSubmit={handleFormSubmit} />
+
+                        <GameImage gameImage={gameImage} setCoords={setCoords} setImageWidth={setImageWidth} setImageHeight={setImageHeight} />
+
+                        <SelectOptions x={clickCoords.x} y={clickCoords.y} z={popupZIndex} handleFormSubmit={handleFormSubmit} />
+
                     </div>
                 </main>
 
@@ -116,6 +138,38 @@ function GameInstructions() {
     )
 }
 
+// game image component
+function GameImage({ gameImage, setCoords, setImageWidth, setImageHeight }) {
+    const ref = useRef(null)
+
+    // get image width and height
+    useLayoutEffect(() => {
+        setImageWidth(ref.current.offsetWidth);
+        setImageHeight(ref.current.offsetHeight);
+    }, []);
+
+    // handle resizing of window
+    useEffect(() => {
+        function handleWindowResize() {
+            setImageWidth(ref.current.offsetWidth);
+            setImageHeight(ref.current.offsetHeight);
+        }
+
+        window.addEventListener('resize', handleWindowResize);
+
+        // clean up when component unmounts
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
+    return (
+        <picture className="w-full h-full" onClick={(e) => setCoords(e)}>
+            <Image ref={ref} src={gameImage.src} alt="" width={800} height={800} className="w-full h-full" />
+        </picture>
+    )
+}
+
 
 
 // on submit check if correct and display result*
@@ -132,31 +186,3 @@ function SelectOptions({ x, y, z, handleFormSubmit }) {
     )
 }
 
-// game timer
-function GameTimer({ gameStart, gamePause }) {
-
-    const [time, setTime] = useState(0);
-    // const [running, setRunning] = useState(true);
-    useEffect(() => {
-        let interval;
-        if (!gamePause) {
-            interval = setInterval(() => {
-                setTime((prevTime) => prevTime + 10);
-            }, 10);
-        } else if (gamePause) {
-            clearInterval(interval);
-        }
-        return () => clearInterval(interval);
-    }, [gamePause]);
-
-   
-    return (
-        <>
-            <div>
-                <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-                <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
-            </div>
-        </>
-    )
-}
