2f08c7e8b5809cae33f87b4cce85124dfa7653f3
Mauritz Marais <mauritz303@gmail.com>
2023-11-02 17:11:53 +0200

    created leaderboard page component

---
+++ src/app/leaderboard/page.js
@@ -0,0 +1,21 @@
+import Nav from "../components/nav"
+
+export default function LeaderboardPage() {
+
+    // leaderboard state*
+    
+    // handle button click*
+    // fetch request with image name to get leaderboard*
+
+    return (
+        <>
+        <Nav />
+            <h1>Leaderboards page</h1>
+            <div>
+                <button>Image 1</button>
+                <button>Image 2</button>
+                <button>Image 3</button>
+            </div>
+        </>
+    )
+}
