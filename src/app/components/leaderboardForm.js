815bd06b1f723c248f0bcf9957af448fd3f69c44
Mauritz Marais <mauritz303@gmail.com>
2023-11-02 17:09:16 +0200

    implemented submit function to update name, added updated state to start new game or view leaderboards

--- src/app/components/leaderboardForm.js
+++ src/app/components/leaderboardForm.js
@@ -1,32 +1,76 @@
+import { useState } from "react";
+import Link from "next/link";
 
-export default function LeaderboardForm({ gameInstance }) {
-    // function to handlesubmit*
-    // send form data*
+export default function LeaderboardForm({ gameInstance, handleNewGame }) {
+
+    const [playerName, setPlayerName] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [nameUpdated, setNameUpdated] = useState(false);
+
+    // input onchange value
+    function handleChange(event) {
+        setPlayerName(event.target.value)
+    };
+
+    // function to handlesubmit
     async function handleSubmit(event, gameInstance) {
         event.preventDefault();
 
         try {
-            // get form data*
+            // get form data
+            const form = event.target;
 
-            // post request* to /leaderboard
-        } 
+            // post request to /leaderboard -> send player_name and game id
+            const response = await fetch('http://localhost:3002/leaderboard', {
+                method: form.method,
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify({ name: playerName, id: gameInstance.id })
+            });
+
+            const result = await response.json();
+
+            // if validation errors
+            if (result.errors) {
+                setInputError(result.errors[0].msg)
+                return;
+            }
+
+            // no errors update state
+            setNameUpdated(true);
+
+        }
         catch (error) {
-           
             console.log(error)
         }
-
     }
-    return (
-        <>
-            <h2>Enter your name to submit your time</h2>
-            <form onSubmit={(e) => handleSubmit(e, gameInstance)} className="flex flex-col items-center gap-4 max-w-md p-4 border-solid border-2">
-                <div className="flex flex-col gap-2 w-3/5">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name" id="name" className="border-solid border-2 border-black-600" />
-                </div>
 
-                <button className="w-3/5">Submit</button>
-            </form>
-        </>
-    )
+    if (nameUpdated) {
+        return (
+            <>
+                <p>Your name has been updated. Thank you for playing.</p>
+                <Link href='/leaderboard'>View leaderboards</Link>
+                <button onClick={handleNewGame}>Start a new game</button>
+            </>
+        )
+    }
+     else {
+        return (
+            <>
+                <h2>Enter your name to submit your time</h2>
+                <form onSubmit={(e) => handleSubmit(e, gameInstance)} className="flex flex-col items-center gap-4 max-w-md p-4 border-solid border-2" method="post">
+
+                    <div className="flex flex-col gap-2 w-3/5">
+                        <label htmlFor="name">Name</label>
+                        <span>(1-20 characters)</span>
+                        <input type="text" name="name" id="name" className="border-solid border-2 border-black-600" value={playerName} onChange={handleChange} />
+                        <span>{inputError}</span>
+                    </div>
+
+                    <button className="w-3/5">Submit</button>
+                </form>
+            </>
+        )
+    }
 }
