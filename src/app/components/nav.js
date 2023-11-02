e1e4cb92a65e8f457f34d42b2ca4319277764a15
Mauritz Marais <mauritz303@gmail.com>
2023-11-02 17:11:09 +0200

    added Link component

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -1,11 +1,12 @@
+import Link from "next/link"
 
 export default function Nav(){
     return (
         <nav className="flex justify-center p-4">
             <ul  className="flex  gap-10">
-                <li><a href="/">Home</a></li>
-                <li><a href="/game">Game</a></li>
-                <li>Leaderboards</li>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/game">Game</Link></li>
+                <li><Link href="/leaderboard">Leaderboards</Link></li>
             </ul>
         </nav>
     )
