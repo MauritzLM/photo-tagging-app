c6267dc5f63a37dedc7eb22daea312784d26be64
Mauritz Marais <mauritz303@gmail.com>
2023-11-06 16:50:45 +0200

    created file to store image data

---
+++ src/utils/imageData.js
@@ -0,0 +1,19 @@
+
+export const image1 = {
+    src: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/image-1.jpg',
+    name: 'image1',
+    characters: ['walrus', 'lion', 'bear']
+}
+
+export const image2 = {
+    src: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/image-2.jpg',
+    name: 'image2',
+    characters: ['char1', 'char2', 'char3']
+}
+
+export const image3 = {
+    src: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/image-3.jpg',
+    name: 'image3',
+    characters: ['char4', 'char5', 'char6']
+}
+
