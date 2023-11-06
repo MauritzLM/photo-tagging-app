4d6d1fc55e77d1118f9f654c42aae2b674b97f9c
Mauritz Marais <mauritz303@gmail.com>
2023-11-06 16:41:16 +0200

    added image configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'phototag-app-images.s3.eu-north-1.amazonaws.com',
+          port: '',
+          pathname: '/image-*.jpg',
+        },
+      ],
+    },
+  }
+
+//   https://get-images-653814252625.s3-accesspoint.eu-north-1.amazonaws.com/image-1.jpg
