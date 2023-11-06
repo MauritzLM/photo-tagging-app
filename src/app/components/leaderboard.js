126204cd2b533ffaf6654eefd981bd7aa4320203
Mauritz Marais <mauritz303@gmail.com>
2023-11-06 16:43:03 +0200

    updated function to map over array and display content

--- src/app/components/leaderboard.js
+++ src/app/components/leaderboard.js
@@ -1,9 +1,27 @@
 
-export default function Leaderboard() {
-    // function to fetch leaderboard*
-    return(
-        <>
-        <h1>Leaderboard</h1>
-        </>
-    )
+export default function DisplayLeaderboard({ leaderboard }) {
+
+    if(leaderboard) {
+        return (
+            <>
+                <h1>Leaderboard</h1>
+                <ol>
+                    {leaderboard.map(player => 
+                        <li key={player.time}>
+                            {player.player_name} - {player.time}
+                            </li>
+                    )}
+                </ol>
+            </>
+        )
+    } else {
+        return (
+            <>
+            <h1>Leaderboard</h1>
+            <p>fetching data...</p>
+            </>
+        )
+    }
+
+   
 }
