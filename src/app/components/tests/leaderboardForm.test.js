33cfad3f25f1d33f0e2e8207cf952251f065e441
Mauritz Marais <mauritz303@gmail.com>
2023-11-09 16:19:18 +0200

    added tests for form submission and state change

--- src/app/components/tests/leaderboardForm.test.js
+++ src/app/components/tests/leaderboardForm.test.js
@@ -2,31 +2,65 @@ import { queryByTestId, render, screen } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom';
 import LeaderboardForm from '../leaderboardForm';
+import { mockFetch, mockFetchError } from './mocks/mock-fetch';
 
-// props: handlenewgame, gameinstance
-// handle submit function -> changes state*
-// value of input
-// mock fetch
+
+// submit function?
 // mocks
 let handleNewGameMock = jest.fn();
 let gameInstanceMock = {id: 1, time: 10000};
-let handleFormSubmit = jest.fn().mockImplementation((e) => e.preventDefault());
-let fetch = jest.fn();
 
-describe('Test leaderboard form rendering and submission', () => {
-    test('renders correctly', () => {
-       render(<LeaderboardForm handleNewGame={handleNewGameMock} gameInstance={gameInstanceMock}/>)
+describe('Leaderboard form', () => {
 
+    beforeEach(() => {
+        render(<LeaderboardForm handleNewGame={handleNewGameMock} gameInstance={gameInstanceMock}/>)
+      });
+
+       test('renders correctly', () => {
        expect(screen.getByTestId('leaderboard-form')).toBeInTheDocument();
     });
 
-    test('submitting form changes state', async () => {
+    // valid submission and state change
+    describe('submitting form and new game', () => {
+        // setup
         const user = userEvent.setup();
 
-        render(<LeaderboardForm handleNewGame={handleNewGameMock} gameInstance={gameInstanceMock}/>);
+        beforeEach(async () => {
+            window.fetch = mockFetch('name');
+            
+            await user.click(screen.getByRole('button'));
+        });
 
-        await user.click(screen.getByRole('button'));
+        // state change
+        test('submitting form changes state', async () => {
+            expect(screen.getByText('Start a new game')).toBeInTheDocument();
+        });
 
-        expect(handleFormSubmit).toHaveBeenCalled();
+        // valid submission
+        test('clicking new game button calls function correctly', async () => {
+            const button = screen.getByText('Start a new game');
+
+            await user.click(button);
+
+            expect(handleNewGameMock).toHaveBeenCalled();
+        });
     });
+
+    // invalid submission
+    describe('Failed form validation', () => {
+        //setup
+        const user = userEvent.setup();
+
+        beforeEach(async () => {
+            window.fetch = mockFetchError('');
+            
+            await user.click(screen.getByRole('button'));
+        });
+
+        // error display
+        test('form error display', () => {
+            expect(screen.getByTestId('error-msg').textContent).toMatch(/please enter a name/i);
+        });
+    });
+   
 });
