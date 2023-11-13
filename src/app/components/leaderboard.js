93a6d0395dd260993ff99b69afee20bf4b59009a
Mauritz Marais <mauritz303@gmail.com>
2023-11-13 17:04:45 +0200

    changed list to table, added conditional styles

--- src/app/components/leaderboard.js
+++ src/app/components/leaderboard.js
@@ -1,10 +1,29 @@
 
-export default function DisplayLeaderboard({ leaderboard }) {
+export default function DisplayLeaderboard({ leaderboard, showLeaderboard }) {
 
     return (
         <>
-            <h1>Leaderboard</h1>
-            <ol>
+            <table className={showLeaderboard === '' ? 'hidden' : 'bg-slate-500'}>
+                <thead>
+                    <tr>
+                        <th className="p-2">Player</th>
+                        <th className="p-2">Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {leaderboard?.map(player =>
+                        <tr key={player.time}>
+                            <td className="p-2">{player.player_name}</td>
+                            <td className="p-2">{("0" + Math.floor((player.time / 60000) % 60)).slice(-2)}:{("0" + Math.floor((player.time / 1000) % 60)).slice(-2)}:{("0" + ((player.time / 10) % 100)).slice(-2)}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+{/*<ol>
                 {leaderboard?.map(player =>
                     <li key={player.time}>
                         {player.player_name} - <span>{("0" + Math.floor((player.time / 60000) % 60)).slice(-2)}:</span>
@@ -12,7 +31,4 @@ export default function DisplayLeaderboard({ leaderboard }) {
                         <span>{("0" + ((player.time / 10) % 100)).slice(-2)}</span>
                     </li>
                 )}
-            </ol>
-        </>
-    )
-}
+            </ol> */}
