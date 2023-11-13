c93a8f35e041422d954e25a1c2c201c2239cc645
Mauritz Marais <mauritz303@gmail.com>
2023-11-13 17:05:30 +0200

    added setup to each test

--- src/app/components/tests/leaderboardForm.test.js
+++ src/app/components/tests/leaderboardForm.test.js
@@ -8,39 +8,43 @@ import { mockFetch, mockFetchError } from './mocks/mock-fetch';
 // submit function?
 // mocks
 let handleNewGameMock = jest.fn();
-let gameInstanceMock = {id: 1, time: 10000};
+let gameInstanceMock = { id: 1, time: 10000 };
 
 describe('Leaderboard form', () => {
 
-    beforeEach(() => {
-        render(<LeaderboardForm handleNewGame={handleNewGameMock} gameInstance={gameInstanceMock}/>)
-      });
-
-       test('renders correctly', () => {
-       expect(screen.getByTestId('leaderboard-form')).toBeInTheDocument();
+    test('renders correctly', () => {
+        render(<LeaderboardForm handleNewGame={handleNewGameMock} gameInstance={gameInstanceMock} />);
+        expect(screen.getByTestId('leaderboard-form')).toBeInTheDocument();
     });
 
     // valid submission and state change
     describe('submitting form and new game', () => {
-        // setup
-        const user = userEvent.setup();
-
+      
         beforeEach(async () => {
             window.fetch = mockFetch('name');
-            
-            await user.click(screen.getByRole('button'));
         });
 
         // state change
         test('submitting form changes state', async () => {
+            const user = userEvent.setup();
+            render(<LeaderboardForm handleNewGame={handleNewGameMock} gameInstance={gameInstanceMock} />);
+            const submitButton = screen.getByRole('button');
+            await user.click(submitButton);
             expect(screen.getByText('Start a new game')).toBeInTheDocument();
         });
 
         // valid submission
         test('clicking new game button calls function correctly', async () => {
-            const button = screen.getByText('Start a new game');
+            const user = userEvent.setup();
 
-            await user.click(button);
+            render(<LeaderboardForm handleNewGame={handleNewGameMock} gameInstance={gameInstanceMock} />);
+            
+            const submitButton = screen.getByRole('button');
+            await user.click(submitButton);
+
+            const newGameButton = screen.getByText('Start a new game');
+
+            await user.click(newGameButton);
 
             expect(handleNewGameMock).toHaveBeenCalled();
         });
@@ -49,18 +53,18 @@ describe('Leaderboard form', () => {
     // invalid submission
     describe('Failed form validation', () => {
         //setup
-        const user = userEvent.setup();
-
         beforeEach(async () => {
             window.fetch = mockFetchError('');
-            
-            await user.click(screen.getByRole('button'));
         });
 
         // error display
-        test('form error display', () => {
+        test('form error display', async () => {
+            const user = userEvent.setup();
+            render(<LeaderboardForm handleNewGame={handleNewGameMock} gameInstance={gameInstanceMock} />);
+        
+            await user.click(screen.getByRole('button'));
+
             expect(screen.getByTestId('error-msg').textContent).toMatch(/please enter a name/i);
         });
     });
-   
 });
