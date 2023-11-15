385acedc050fc23133c94a5d20e79e3c4e214ce2
Mauritz Marais <mauritz303@gmail.com>
2023-11-15 16:55:03 +0200

    removed split second display, updated font styles

--- src/app/components/timer.js
+++ src/app/components/timer.js
@@ -18,10 +18,10 @@ export default function GameTimer({ gameStart}) {
 
     return (
         <>
-            <div>
+            <div className="text-4xl font-medium">
                 <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-                <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+                {/* <span>{("0" + ((time / 10) % 100)).slice(-2)}</span> */}
             </div>
         </>
     )
