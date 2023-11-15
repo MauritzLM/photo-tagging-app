d6b607dabb13596ce21403cc6d9af29083f4ceb5
Mauritz Marais <mauritz303@gmail.com>
2023-11-15 16:56:03 +0200

    updated layout and button styles

--- src/app/leaderboard/page.js
+++ src/app/leaderboard/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from "react";
-import Nav from "../components/nav"
+import Nav from "../components/nav";
 import DisplayLeaderboard from "../components/leaderboard";
 
 export default function LeaderboardPage() {
@@ -36,17 +36,18 @@ export default function LeaderboardPage() {
     return (
         <>
             <Nav />
-            <h1>Leaderboards page</h1>
+            
+            <main className="flex flex-col items-center gap-5 p-4">
+            <h1 className="font-bold text-2xl text-center my-5">Select a leaderboard to display</h1>
 
-            <p>Select a leaderboard to display</p>
-
-            <div>
-                <button className={showLeaderboard === 'image1' ? 'bg-slate-500 text-white' : ''} onClick={() => getLeaderboard('image1')}>Image 1</button>
-                <button className={showLeaderboard === 'image2' ? 'bg-slate-500 text-white' : ''} onClick={() => getLeaderboard('image2')}>Image 2</button>
-                <button className={showLeaderboard === 'image3' ? 'bg-slate-500 text-white' : ''}>Image 3</button>
+            <div className="flex items-center gap-5">
+                <button className={showLeaderboard === 'image1' ? 'bg-slate-500 text-white p-3 border border-black rounded-lg' : ' p-3 border border-black rounded-lg'} onClick={() => getLeaderboard('image1')}>Image 1</button>
+                <button className={showLeaderboard === 'image2' ? 'bg-slate-500 text-white p-3 border border-black rounded-lg' : ' p-3 border border-black rounded-lg'} onClick={() => getLeaderboard('image2')}>Image 2</button>
+                <button className={showLeaderboard === 'image3' ? 'bg-slate-500 text-white p-3 border border-black rounded-lg' : ' p-3 border border-black rounded-lg'} onClick={() => getLeaderboard('image3')}>Image 3</button>
             </div>
 
             <DisplayLeaderboard leaderboard={leaderboard} showLeaderboard={showLeaderboard}/>
+            </main>
         </>
     )
 }
