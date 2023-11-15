b2cb601ca2a4bbbe3a891f1c971e720bf6ff073f
Mauritz Marais <mauritz303@gmail.com>
2023-11-15 16:51:57 +0200

    updated bg-color and padding

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function Nav(){
     return (
-        <nav className="flex justify-center p-4">
+        <nav className="flex justify-center p-5 bg-slate-800 text-white">
             <ul  className="flex  gap-10">
                 <li><Link href="/">Home</Link></li>
-                <li><Link href="/game">Game</Link></li>
+                <li><Link href="/game">Play</Link></li>
                 <li><Link href="/leaderboard">Leaderboards</Link></li>
             </ul>
         </nav>
