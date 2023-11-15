8f499638e54382497acd77f4cee0df499c85e9fa
Mauritz Marais <mauritz303@gmail.com>
2023-11-15 16:52:30 +0200

    added image source to characters

--- src/utils/imageData.js
+++ src/utils/imageData.js
@@ -2,18 +2,24 @@
 export const image1 = {
     src: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/image-1.jpg',
     name: 'image1',
-    characters: ['walrus', 'guy in blue', 'tiger woods']
-}
+    characters: [{name: 'walrus', imageSrc: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/walrus.png'},
+     {name: 'guy in blue', imageSrc: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/blueshirt.png'},
+      {name: 'tiger woods', imageSrc: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/tiger.png'}]
+};
 
 export const image2 = {
     src: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/image-2.jpg',
     name: 'image2',
-    characters: ['chameleon', 'bunny', 'beanie guy']
-}
+    characters: [{name: 'chameleon', imageSrc: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/chameleon.png'},
+     {name: 'bunny', imageSrc: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/bunny.png'},
+      {name: 'beanie guy', imageSrc: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/beanie.png'}]
+};
 
 export const image3 = {
     src: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/image-3.jpg',
     name: 'image3',
-    characters: ['char4', 'char5', 'char6']
-}
+    characters: [{name:'wally', imageSrc: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/wally.png'},
+     {name:'gingerbread man', imageSrc: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/gingerbreadman.png'}, 
+     {name: 'blue hat', imageSrc: 'https://phototag-app-images.s3.eu-north-1.amazonaws.com/bluehat.png'}]
+};
 
