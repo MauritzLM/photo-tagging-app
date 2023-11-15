ce25cab3940fa0c4ee26c710357cd49136d313f4
Mauritz Marais <mauritz303@gmail.com>
2023-11-15 16:51:21 +0200

    updated styles

--- src/app/components/leaderboard.js
+++ src/app/components/leaderboard.js
@@ -3,18 +3,18 @@ export default function DisplayLeaderboard({ leaderboard, showLeaderboard }) {
 
     return (
         <>
-            <table className={showLeaderboard === '' ? 'hidden' : 'bg-slate-500'}>
+            <table className={showLeaderboard === '' ? 'hidden' : 'bg-slate-500 text-white w-full md:w-3/4 border-2 border-black'}>
                 <thead>
-                    <tr>
+                    <tr className="border-b">
                         <th className="p-2">Player</th>
                         <th className="p-2">Time</th>
                     </tr>
                 </thead>
                 <tbody>
                     {leaderboard?.map(player =>
-                        <tr key={player.time}>
-                            <td className="p-2">{player.player_name}</td>
-                            <td className="p-2">{("0" + Math.floor((player.time / 60000) % 60)).slice(-2)}:{("0" + Math.floor((player.time / 1000) % 60)).slice(-2)}:{("0" + ((player.time / 10) % 100)).slice(-2)}</td>
+                        <tr className="border-b" key={player.time}>
+                            <td className="p-2 text-center">{player.player_name}</td>
+                            <td className="p-2 text-center">{("0" + Math.floor((player.time / 60000) % 60)).slice(-2)}:{("0" + Math.floor((player.time / 1000) % 60)).slice(-2)}:{("0" + ((player.time / 10) % 100)).slice(-2)}</td>
                         </tr>
                     )}
                 </tbody>
@@ -22,13 +22,3 @@ export default function DisplayLeaderboard({ leaderboard, showLeaderboard }) {
         </>
     )
 }
-
-{/*<ol>
-                {leaderboard?.map(player =>
-                    <li key={player.time}>
-                        {player.player_name} - <span>{("0" + Math.floor((player.time / 60000) % 60)).slice(-2)}:</span>
-                        <span>{("0" + Math.floor((player.time / 1000) % 60)).slice(-2)}:</span>
-                        <span>{("0" + ((player.time / 10) % 100)).slice(-2)}</span>
-                    </li>
-                )}
-            </ol> */}
