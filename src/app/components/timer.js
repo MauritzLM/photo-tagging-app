4757f31d4defd5427e7f044b29dca60cf99ee11b
Mauritz Marais <mauritz303@gmail.com>
2023-11-16 17:15:37 +0200

    increased width of timer

--- src/app/components/timer.js
+++ src/app/components/timer.js
@@ -18,7 +18,7 @@ export default function GameTimer({ gameStart}) {
 
     return (
         <>
-            <div className="text-4xl font-medium">
+            <div className="text-4xl font-medium w-[6rem]">
                 <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
                 <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
                 {/* <span>{("0" + ((time / 10) % 100)).slice(-2)}</span> */}
