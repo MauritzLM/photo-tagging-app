4bf35c375edff485479e3bc44860be5fe6af0dad
Mauritz Marais <mauritz303@gmail.com>
2023-11-16 17:12:53 +0200

    updated spacing of elements

--- src/app/page.js
+++ src/app/page.js
@@ -8,15 +8,15 @@ export default function Home() {
       <Nav />
       <main className="flex flex-col items-center gap-5">
 
-        <h1 className="text-center font-bold text-4xl my-5">Photo tagging app</h1>
+        <h1 className="text-center font-bold text-4xl my-12">Photo tagging app</h1>
 
-        <section className="max-w-[70%] flex flex-col items-center">
+        <section className="max-w-[70%] flex flex-col items-center gap-5">
 
           <h2 className='font-bold text-2xl my-2'>Game instructions</h2>
           <ol className="p-4 list-decimal">
-            <li>On the <Link className='underline text-blue-500' href="/game">game page</Link> select an image</li>
-            <li>Find all three characters by clicking on them and seleting the correct option</li>
-            <li>Once you have found all three characters the game is over and you have an option to add your name to the leaderboard</li>
+            <li className="my-2">On the <Link className='underline text-blue-500' href="/game">game page</Link> select an image</li>
+            <li className="my-2">Find all three characters by clicking on them and selecting the correct option</li>
+            <li className="my-2">Once you have found all three characters the game is over and you have an option to add your name to the leaderboard</li>
           </ol>
 
           <p className="font-bold">Thanks for playing!</p>
