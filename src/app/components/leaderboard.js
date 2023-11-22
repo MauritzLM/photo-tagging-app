f68fb3decc7790da18f76c3045d3fe87acf0a5d4
Mauritz Marais <mauritz303@gmail.com>
2023-11-22 13:04:25 +0200

    added data-testids

--- src/app/components/leaderboard.js
+++ src/app/components/leaderboard.js
@@ -5,14 +5,14 @@ export default function DisplayLeaderboard({ leaderboard, showLeaderboard }) {
         <>
             <table className={showLeaderboard === '' ? 'hidden' : 'w-full md:w-3/4 lg:w-1/2 text-lg rounded-sm text-text-secondary'}>
                 <thead>
-                    <tr className="border-b border-secondary">
+                    <tr data-testid="row" className="border-b border-secondary">
                         <th className="p-2">Player</th>
                         <th className="p-2">Time</th>
                     </tr>
                 </thead>
                 <tbody>
                     {leaderboard?.map(player =>
-                        <tr className="border-b border-secondary" key={player.time}>
+                        <tr data-testid="row" className="border-b border-secondary" key={player.time}>
                             <td className="p-2 text-center">{player.player_name}</td>
                             <td className="p-2 text-center">{("0" + Math.floor((player.time / 60000) % 60)).slice(-2)}:{("0" + Math.floor((player.time / 1000) % 60)).slice(-2)}:{("0" + ((player.time / 10) % 100)).slice(-2)}</td>
                         </tr>
