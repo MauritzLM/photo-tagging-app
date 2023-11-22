f264b704572f6a9961c2d2450747202fbb278228
Mauritz Marais <mauritz303@gmail.com>
2023-11-22 13:07:12 +0200

    changed test assertion

--- src/app/tests/homepage.test.js
+++ src/app/tests/homepage.test.js
@@ -1,9 +1,11 @@
-import {render, screen} from '@testing-library/react';
+import {getAllByRole, render, screen} from '@testing-library/react';
 import Home from '../page';
 
 // rendering
 test('renders home page', () => {
-    render(<Home />)
+    render(<Home />);
 
-    expect(screen.getByRole('heading').textContent).toBe('Photo tagging app')
+    const headings = screen.getAllByRole('heading');
+
+    expect(headings[0].textContent).toBe('Photo tagging app');
 });
