a1c96773c856e712ad161342581255f91b21f0a2
Mauritz Marais <mauritz303@gmail.com>
2023-12-08 11:49:07 +0200

    removed sticky nav

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -4,7 +4,7 @@ export default function Nav({ currentPage }) {
     return (
         <>
         <div></div>
-        <nav className="flex justify-center p-5 text-text-secondary shadow-sm sticky top-0 z-10 bg-secondary text-lg">
+        <nav className="flex justify-center p-5 text-text-secondary shadow-sm top-0 z-10 bg-secondary text-lg">
             <ul className="flex  gap-10">
                 <li className={currentPage === 'home' ? 'border-b-2 border-text-secondary font-semibold' : 'hover:border-b-2 border-text-secondary transition-all'}><Link href="/">Home</Link></li>
                 <li className={currentPage === 'game' ? 'border-b-2 border-text-secondary font-semibold' : 'hover:border-b-2 border-text-secondary transition-all'}><Link href="/game">Play</Link></li>
